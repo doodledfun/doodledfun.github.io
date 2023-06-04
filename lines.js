623c45cee47efc54063d768ab9d3f00d61b1f978
Elof B <135530281+doodledfun@users.noreply.github.com>
2023-06-04 19:12:35 +0100

    Update lines.js

--- lines.js
+++ lines.js
@@ -1,10 +1,10 @@
 const sentenceStructures = [
-    "I will not {action} the {object} during {class}.",
-    "I promise to keep my {pet} away from my {object} in {class}.",
-    "I will not {action} the {food} during {class}.",
-    "I promise to keep my {pet} away from the {food} in {class}.",
-    "I will not {action} {food} during {class}.",
-    "I will not {action} {object} during {class}."
+    "I will not {action} the {object} during {class}",
+    "I promise to keep my {pet} away from my {object} in {class}",
+    "I will not {action} the {food} during {class}",
+    "I promise to keep my {pet} away from the {food} in {class}",
+    "I will not {action} {food} during {class}",
+    "I will not {action} {object} during {class}"
 ];
 
 const objects = [
